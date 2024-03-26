3797aa4ecf3eb9fa78e53bb7fc2913e052b7555c
marcgithub23 <marc.dagohoy2@gmail.com>
2024-03-26 18:48:36 +0000

    Deploy project early to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn readsreviews.wsgi
--- readsreviews/settings.py
+++ readsreviews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7f*5+7&+w6d63z#w+ueird@+bkx(zy6)bn@2ln97@!s#5#5nga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
