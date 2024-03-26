90b918172f58634c6f1d9ea431c6e87bb6b70999
marcgithub23 <marc.dagohoy2@gmail.com>
2024-03-26 22:17:37 +0000

    Secure secret key and redeploy to Heroku

--- readsreviews/settings.py
+++ readsreviews/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7f*5+7&+w6d63z#w+ueird@+bkx(zy6)bn@2ln97@!s#5#5nga'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
