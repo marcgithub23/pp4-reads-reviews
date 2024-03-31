d6cb7d79e0a4c836a1d3da984933148bebeb9aa3
marcgithub23 <marc.dagohoy2@gmail.com>
2024-03-31 15:31:04 +0100

    Implement summernote in admin panel

--- review/admin.py
+++ review/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import BookReview, Comment
+
+
+@admin.register(BookReview)
+class BookReviewAdmin(SummernoteModelAdmin):
+
+    list_display = ('book_title', 'slug', 'status')
+    search_fields = ['book_title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('reviewer','book_title',)}
+    summernote_fields = ('book_review',)
+
 
 # Register your models here.
 
-admin.site.register(BookReview)
 admin.site.register(Comment)
