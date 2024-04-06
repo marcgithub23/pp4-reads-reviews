ca634663a5187910b5ac55b181e843c372da63e1
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-06 22:49:41 +0100

    Add review_detail view

--- review/urls.py
+++ review/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.BookReviewList.as_view(), name='home'),
+    path('<slug:slug>/', views.review_detail, name='review_detail'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import BookReview
 
@@ -9,3 +9,27 @@ class BookReviewList(generic.ListView):
     queryset = BookReview.objects.filter(status=1)
     template_name = "review/index.html"
     paginate_by = 6
+
+
+def review_detail(request, slug):
+    """
+    Display an individual :model:`review.BookReview`.
+
+    **Context**
+
+    ``review``
+        An instance of :model:`review.BookReview`.
+
+    **Template:**
+
+    :template:`review/review_detail.html`
+    """
+
+    queryset = BookReview.objects.filter(status=1)
+    book_review = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "review/review_detail.html",
+        {"review": book_review},
+    )
