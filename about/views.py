374d75ade9f9b50a74c78dfafa60c3a3280cf7ba
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-08 11:59:36 +0100

    Add about page

---
+++ about/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
+
+
+def about_website(request):
+    """
+    Renders the About page
+    """
+
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
--- readsreviews/settings.py
+++ readsreviews/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'review',
+    'about',
 ]
 
 MIDDLEWARE = [
