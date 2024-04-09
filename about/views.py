facf1b7eb3dbee1bfdfda7cfd4033d95a3419318
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-09 22:07:54 +0100

    Handle post method for feedback form and render form on about page

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import About
+from django.contrib import messages
+from .models import About, Feedback
+from .forms import FeedbackForm
 
 # Create your views here.
 
@@ -9,10 +11,24 @@ def about_website(request):
     Renders the About page
     """
 
+    if request.method == "POST":
+        feedback_form = FeedbackForm(data=request.POST)
+        if feedback_form.is_valid():
+            feedback_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Feedback received! Thank you for your support."
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+    feedback_form = FeedbackForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "feedback_form": feedback_form
+        },
     )
