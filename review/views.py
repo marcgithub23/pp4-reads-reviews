af418abd08740ab50c20fb5bb9453b933837d8db
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-09 15:03:25 +0100

    Implement comment form and display confirmation message

--- review/views.py
+++ review/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import BookReview
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -30,6 +32,20 @@ def review_detail(request, slug):
     comments = book_review.comments.all().order_by("-created_on")
     comment_count = book_review.comments.all().count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.commenter = request.user
+            comment.book_review = book_review
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted'
+            )
+
+    comment_form = CommentForm()
+
     return render(
         request,
         "review/review_detail.html",
@@ -37,5 +53,6 @@ def review_detail(request, slug):
             "review": book_review,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
