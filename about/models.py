1c43b95922a1cb319bca7c1f7600b1bf5f19ba27
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-09 22:04:58 +0100

    Create and migrate feedback model

--- about/models.py
+++ about/models.py
@@ -13,3 +13,17 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Feedback(models.Model):
+    """
+    Stores a single feedback message
+    """
+
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Feedback from {self.name}"
