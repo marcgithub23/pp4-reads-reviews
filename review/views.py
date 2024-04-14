28f8d1cc770e7a0be352a6f8836bb6182af2adda
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-14 12:29:07 +0100

    Implement edit and delete features for comments

--- review/views.py
+++ review/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import BookReview
+from django.http import HttpResponseRedirect
+from .models import BookReview, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -56,3 +57,54 @@ def review_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    View to edit comments
+    """
+
+    if request.method == "POST":
+        queryset = BookReview.objects.filter(status=1)
+        book_review = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.commenter == request.user:
+            comment = comment_form.save(commit=False)
+            comment.book_review = book_review
+            comment.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Comment Updated!'
+            )
+        else:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'Error updating comment!'
+            )
+
+    return HttpResponseRedirect(reverse('review_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    View to delete comment
+    """
+
+    queryset = BookReview.objects.filter(status=1)
+    book_review = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.commenter == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'You can only delete your own comments!'
+        )
+
+    return HttpResponseRedirect(reverse('review_detail', args=[slug]))
