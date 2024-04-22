1c8a86ee8e548d01003822b742379555cb8e4c75
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-22 12:33:24 +0100

    Register UserProfile in admin

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import UserProfile
+
 # Register your models here.
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin for the UserProfile model."""
+
+    list_display = ('user_profile', 'profile_photo', 'bio')
