fd7e149c757fc755c457b5c160911d553a2f7043
marcgithub23 <marc.dagohoy2@gmail.com>
2024-04-27 19:09:31 +0100

    Delete manual profile creation feature

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -12,15 +12,3 @@ class EditAccountForm(forms.ModelForm):
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'})
         }
-
-
-class CreateProfilePageForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['profile_photo', 'bio']
-        widgets = {
-            'profile_photo': forms.FileInput(
-                attrs={'class':'form-control', 'type':'file'}
-            ),
-            'bio': forms.Textarea(attrs={'class':'form-control'}),
-        }
